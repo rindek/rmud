54a11a7d5fea19970e3e7bf751df84e0af8604e3
Jacek Jakubik <jakubik.jacek@gmail.com>
2011-06-28 22:08:32 +0200

    database schema

--- doc/rmud.sql
+++ doc/rmud.sql
@@ -6,4 +6,43 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `password` text NOT NULL,
   `player_password` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+
+CREATE TABLE IF NOT EXISTS `declension` (
+  `nazwa` varchar(150) NOT NULL,
+  `mianownik` varchar(150) NOT NULL,
+  `dopelniacz` varchar(150) NOT NULL,
+  `celownik` varchar(150) NOT NULL,
+  `biernik` varchar(150) NOT NULL,
+  `narzednik` varchar(150) NOT NULL,
+  `miejscownik` varchar(150) NOT NULL,
+  `rodzaj` int(11) NOT NULL,
+  PRIMARY KEY (`nazwa`),
+  FULLTEXT KEY `nazwa` (`nazwa`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `options` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `name` varchar(35) NOT NULL,
+  `created` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+CREATE TABLE IF NOT EXISTS `player_options` (
+  `player_id` int(11) NOT NULL,
+  `option_id` int(11) NOT NULL,
+  `value` text NOT NULL,
+  KEY `player_id` (`player_id`),
+  KEY `option_id` (`option_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
