f531dc059f1da95df4fab239bf37578891bcfd11
Jacek Jakubik <jakubik.jacek@gmail.com>
2012-06-30 13:07:58 +0200

    this should be divided into two apps - webapp and rmud itself

---
+++ new/rmud_webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RmudWebapp::Application.config.secret_token = '355344a895256b2f9df296b04e26c44e469c1c64087865a384af0f509f63bbb95119b3228bed1fa4c94ebdd618fec03e1e07a9fd2a44ad912868b54d061ccb82'
