2bf6bb3237f55a96a3af739d71d31dadf1450327
Jacek Jakubik <jacek@hotcode.pl>
2020-06-01 16:40:03 +0200

    provide any pass...

--- ci.rb
+++ ci.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 require "sequel"
 
-db = Sequel.connect("postgres://postgres@database:5432/postgres")
+db = Sequel.connect("postgres://postgres:x@database:5432/postgres")
 db.execute(%(CREATE DATABASE "rmud_test"))
