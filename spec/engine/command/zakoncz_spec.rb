8333491eda40cc090adb3edde91e67e7f6ad95c4
Jacek Jakubik <jacek@hotcode.pl>
2021-01-06 22:35:05 +0100

    Add tests for Zakoncz command

--- spec/engine/command/zakoncz_spec.rb
+++ spec/engine/command/zakoncz_spec.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
-RSpec.xdescribe Engine::Command::Zakoncz do
+RSpec.describe Engine::Command::Zakoncz do
   let(:command) { described_class.new(client: client, tp: tp) }
 
   subject { command.call }
 
-  let(:client) { instance_double(Engine::Client) }
+  let(:client) { Engine::Client.new(em_connection: FakeSocketClient.new(nil)) }
   let(:tp) { build(:player) }
 
   context "Happy path" do
+    before { expect(client).to receive(:close) }
+
     it { is_expected.to be_successful }
+
+    it "cleans object from game" do
+      expect(command).to receive(:clean_up)
+      subject
+    end
   end
 end
---
+++ spec/support/event_machine/fake_socket_client.rb
@@ -0,0 +1,1 @@
+class FakeSocketClient < EventMachine::Connection; end
