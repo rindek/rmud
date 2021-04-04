3142e086150950dcd9c196753862e7da2008fb7b
Jacek Jakubik <jacek@hotcode.pl>
2021-04-04 15:02:35 +0200

    Remove redundant stuff

--- boot/settings.rb
+++ boot/settings.rb
@@ -12,11 +12,6 @@ App.boot(:settings, from: :system) do
     key :host, T::String.default("0.0.0.0")
     key :port, T::Integer.default(2300)
 
-    key :db_user, T::String.default("postgres")
-    key :db_host, T::String.default("database")
-    key :db_name, T::String.default("rmud_#{App.env}".freeze)
-    key :db_pass, T::String.default("x")
-
     key :mongo_host, T::String.default("mongo")
     key :mongo_port, T::Integer.default(27_017)
     key :mongo_database, T::String.default("rmud_#{App.env}".freeze)
