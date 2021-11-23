387614c09336f47f490855791a8526f6dc0e1ba6
Jacek Jakubik <jacek@hotcode.pl>
2021-11-23 15:12:53 +0100

    interface methods for pg base repo

--- repos/pg.rb
+++ repos/pg.rb
@@ -8,29 +8,29 @@ module Repos
     option :entity, default: -> { raise "entity must be set" }
 
     def first
-      # find_by({})
+      find_by({})
     end
 
     def find_by(args)
-      # dataset.find(args).first.then { |record| record ? Some(wrap(record)) : None() }
+      dataset.where(args).first.then { |record| record ? Some(wrap(record)) : None() }
     end
 
     def all_by(args)
-      # dataset.find(args).to_a.then { |records| records.empty? ? None() : Some(wrap_many(records)) }
+      dataset.where(args).all.then { |records| records.empty? ? None() : Some(wrap_many(records)) }
     end
 
     def each(&block)
-      # dataset.find({}).each { |document| block.call(wrap(document)) }
+      dataset.all.each { |record| block.call(wrap(record)) }
     end
 
     private
 
     def wrap(record)
-      # entity.new(record)
+      entity.new(record)
     end
 
     def wrap_many(records)
-      # records.map { |record| wrap(record) }
+      records.map { |record| wrap(record) }
     end
   end
 end
