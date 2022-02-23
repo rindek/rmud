74b02b4ae956c6b7a536d40d7557134333bab8d1
Jacek Jakubik <jacek@hotcode.pl>
2022-02-23 23:56:06 +0100

    break spec on purpose

--- spec/engine/core_spec.rb
+++ spec/engine/core_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Engine::Core do
 
     before { allow(caller_locations).to receive(:first).and_return(double(path: base_absolute)) }
 
-    it { expect(core.Relative("another_room", base_absolute)).to eq("world.another_room") }
+    it { expect(core.Relative("another_room", base_absolute)).to eq("wold.another_room") }
     it { expect(core.Relative("folder/room", base_absolute)).to eq("world.folder.room") }
     it { expect(core.Relative("../domain/folder/room", base_absolute)).to eq("domain.folder.room") }
   end
